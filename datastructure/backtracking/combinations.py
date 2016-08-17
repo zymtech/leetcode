61c38bf9a80c459c7e89040fcad84e84a845112b
ymzhang <ym_zhang@foxmail.com>
2016-08-18 00:08:28 +0800

    one practise a day, keep bugs away

---
+++ datastructure/backtracking/combinations.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    # @return a list of lists of integers
+    def combine(self, n, k):
+        result = []
+        self.combineRecu(n, result, 0, [], k)
+        return result
+
+    def combineRecu(self, n, result, start, intermediate, k):
+        if k == 0:
+            result.append(intermediate[:])
+            #print result
+        for i in xrange(start, n):
+            intermediate.append(i + 1)
+            self.combineRecu(n, result, i + 1, intermediate, k - 1)
+            intermediate.pop()
+
+if __name__ == "__main__":
+    result = Solution().combine(4, 2)
+    print result
+
