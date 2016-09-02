761f68235012f7d53fa1f93ae3a7323cd800adf4
ymzhang <ym_zhang@foxmail.com>
2016-09-02 11:16:12 +0800

    add non-recursive solution to subsets

--- datastructure/backtracking/subsets.py
+++ datastructure/backtracking/subsets.py
@@ -15,6 +15,21 @@ class Solution(object):
             return [cur]
         return self.recursive(cur, nums[1:]) + self.recursive(cur + [nums[0]],\
                                                               nums[1:])
+
+
+class NoneRecurSolution(object):
+    def subsets(self, nums):
+        result = [[]]
+        #nums.sort()
+        for i in xrange(len(nums)):
+            size = len(result)
+            for j in range(size):
+                result.append(list(result[j]))
+                result[-1].append(nums[i])
+        return result
+
+
 if __name__ == "__main__":
-    nums = [1,3,4]
+    nums = [1,4,3]
     print Solution().subsets(nums)
+    print NoneRecurSolution().subsets(nums)
